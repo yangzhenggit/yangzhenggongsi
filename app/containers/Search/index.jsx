347b50e464c15abde867bb677315a7859a8e8cc8
yangzheng3 <Christina@0801>
2018-06-20 11:45:40 +0800

    loadMore

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 import SearchHeader from '../../components/SearchHeader/index.jsx'
+import SearchList from './subpage/List.jsx'
 
 class Search extends React.Component {
 	constructor(props, context) {
@@ -15,6 +16,7 @@ class Search extends React.Component {
 			//<div>keyword: {params.keyWord},category: {params.category}</div>
 			<div>
 				<SearchHeader keyWord={params.keyWord} />
+				<SearchList keyWord={params.keyWord} category={params.category}/>
 			</div>
 		)
 	}
